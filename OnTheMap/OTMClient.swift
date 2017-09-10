40c20ff77d7354fea722ec89d7b28ecaad81657e
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-10 16:13:00 -0700

    Add OTMClient Constants

---
+++ OnTheMap/OTMClient.swift
@@ -0,0 +1,43 @@
+//
+//  OTMClient.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/10/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OTMClient: NSObject {
+    
+    // MARK: Constants
+    struct Constants {
+        
+        // MARK: API Key & Parse ID
+        static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let ParseApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        
+        // MARK: URLs
+        static let ApiScheme = "https"
+        static let ApiHost = "parse.udacity.com"
+        static let ApiPath = "parse/classes"
+        static let AuthorizationURL = ""
+    }
+    
+    func OTMURLFromParameters(_ parameters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = OTMClient.Constants.ApiScheme
+        components.host = OTMClient.Constants.ApiHost
+        components.path = OTMClient.Constants.ApiPath + (withPathExtension ?? "")
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+}

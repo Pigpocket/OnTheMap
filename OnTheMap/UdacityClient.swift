f4bd5d8996db79542b024031f821906c39b5c0d5
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-10 16:31:00 -0700

    Create Udacity client constants

---
+++ OnTheMap/UdacityClient.swift
@@ -0,0 +1,31 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/10/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UdacityClient: NSObject {
+    
+    // MARK: Constants
+    struct Constants {
+        
+    }
+    
+    struct Method {
+        
+        static let SessionURL = "https://www.udacity.com/api/session"
+    }
+    
+    struct ParameterKeys {
+        
+        static let udacityDict = [String:AnyObject]()
+        static let username = ""
+        static let password = ""
+    }
+    
+}

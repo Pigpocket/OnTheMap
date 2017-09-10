ed05e232da7ab35c255482a89e601532c2db94c9
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-10 13:51:55 -0700

    Add table view cell

---
+++ OnTheMap/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/10/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+}

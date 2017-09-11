387aa51df1730409d26483c66578c6b3f56ecced
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-10 18:17:59 -0700

    Create LoginViewController

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/10/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var login: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}

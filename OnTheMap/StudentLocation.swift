bab0ba9811ff11644a73e547969f6e369d78f925
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-21 21:30:13 -0700

    Add StudentLocation struct

---
+++ OnTheMap/StudentLocation.swift
@@ -0,0 +1,23 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/21/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StudentLocation {
+    
+    let objectID: String
+    let uniqueKey: String
+    let firstName: String
+    let lastName: String
+    let mapString: String
+    let mediaURL: String
+    let latitude: Double
+    let longitude: Double
+    
+}

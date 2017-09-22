d5b7f2d63449aa152bcb0586f335be062808750e
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-21 21:53:44 -0700

    Create taskForGetStudentLocations function with parameters

--- OnTheMap/ParseClient.swift
+++ OnTheMap/ParseClient.swift
@@ -11,4 +11,20 @@ import UIKit
 
 class ParseClient: NSObject {
     
+    
+    func taskForGetStudentLocations(limit: Int, skip: Int, order: String, completionHandlerForGET: @escaping (_ results: AnyObject?, NSError?) -> Void) {
+    
+    let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+    request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+    request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+    let session = URLSession.shared
+    let task = session.dataTask(with: request as URLRequest) { data, response, error in
+        if error != nil { // Handle error...
+            return
+        }
+        print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!)
+    }
+    task.resume()
+}
+
 }

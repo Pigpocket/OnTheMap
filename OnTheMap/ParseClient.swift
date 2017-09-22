67b56c7ca527ace97d4a67c6316a469f12cb396c
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-21 22:16:30 -0700

    Complete task ForGetStudentLocations implementation

--- OnTheMap/ParseClient.swift
+++ OnTheMap/ParseClient.swift
@@ -11,20 +11,68 @@ import UIKit
 
 class ParseClient: NSObject {
     
+    var session = URLSession.shared
     
-    func taskForGetStudentLocations(limit: Int, skip: Int, order: String, completionHandlerForGET: @escaping (_ results: AnyObject?, NSError?) -> Void) {
+    func taskForGetStudentLocations(limit: Int, skip: Int, order: String, completionHandlerForGET: @escaping (_ results: AnyObject?, _ error: NSError?) -> Void) {
     
-    let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
-    request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-    request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-    let session = URLSession.shared
-    let task = session.dataTask(with: request as URLRequest) { data, response, error in
-        if error != nil { // Handle error...
-            return
+        let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+    
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+        
+            /* GUARD: Was there an error */
+            guard error == nil else {
+                let userInfo = [NSLocalizedDescriptionKey: "There was an error with your request: \(error)"]
+                completionHandlerForGET(nil, NSError(domain: "taskForGetStudentLocation", code: 0, userInfo: userInfo))
+                return
+            }
+        
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? HTTPURLResponse {
+                    let userInfo = [NSLocalizedDescriptionKey: "Your request returned an invalid response! Status code: \(response.statusCode)!"]
+                    completionHandlerForGET(nil, NSError(domain: "taskForGetStudentLocation", code: 1, userInfo: userInfo))
+                } else if let response = response {
+                    let userInfo = [NSLocalizedDescriptionKey: "Your request returned an invalid response! Response: \(response)!"]
+                    completionHandlerForGET(nil, NSError(domain: "taskForGetStudentLocation", code: 2, userInfo: userInfo))
+                } else {
+                    let userInfo = [NSLocalizedDescriptionKey: "Your request returned an invalid response!"]
+                    completionHandlerForGET(nil, NSError(domain: "taskForGetStudentLocation", code: 3, userInfo: userInfo))
+                }
+                return
+            }
+        
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                let userInfo = [NSLocalizedDescriptionKey: "No data was returned by the request!"]
+                completionHandlerForGET(nil, NSError(domain: "taskForGetStudentLocation", code: 1, userInfo: userInfo))
+                return
+            }
+        
+            print(NSString(data: data, encoding: String.Encoding.utf8.rawValue)!)
+        
+            /* Parse the data */
+        
+            self.parseJSONObject(data, completionHandlerForConvertData: completionHandlerForGET)
+        
         }
-        print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!)
+    
+        task.resume()
     }
-    task.resume()
-}
+    
+    func parseJSONObject(_ data: Data, completionHandlerForConvertData: (_ results: AnyObject?, _ error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject!
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            print(error)
+            let userInfo = [NSLocalizedDescriptionKey: "Could not parse the data as JSON: \(data)"]
+            completionHandlerForConvertData(nil, NSError(domain: "parseJSONObject", code: 0, userInfo: userInfo))
+        }
+        print("ParseClient parsedResult is \(parsedResult)")
+        completionHandlerForConvertData(parsedResult, nil)
+    }
 
 }

54aee119a1b7b381b5e1516a2f3f400e1ba07eb5
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-21 21:35:50 -0700

    Add ParseClient/modify existing swift file to ParseConstants

---
+++ OnTheMap/ParseConstants.swift
@@ -0,0 +1,43 @@
+//
+//  OTMClient.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/10/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ParseClient {
+    
+    // MARK: Constants
+    struct Constants {
+        
+        // MARK: API Key & Parse ID
+        static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let ParseApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        
+        // MARK: URLs
+        static let ApiScheme = "https"
+        static let ApiHost = "parse.udacity.com"
+        static let ApiPath = "parse/classes"
+        static let AuthorizationURL = ""
+    }
+    
+    func ParseURLFromParameters(_ parameters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = ParseClient.Constants.ApiScheme
+        components.host = ParseClient.Constants.ApiHost
+        components.path = ParseClient.Constants.ApiPath + (withPathExtension ?? "")
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+}

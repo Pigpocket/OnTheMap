cda8a5d37b68dbf7f9fc0a76c5e1bafaa5da5dee
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-26 12:24:44 -0700

    remove postSessionID function from LoginViewController

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -31,15 +31,7 @@ class LoginViewController: UIViewController {
         // guard that username AND passowrd is != ""
         
         UdacityClient.sharedInstance().authenticateUser(email: usernameTextField.text!, password: passwordTextField.text!) { (success, sessionID, error) in
-            //
-            // guard for success
             
-            // get your public user data from Udacity using Udacity API (goal: get first_name, last_name only!)
-            //   in the completion handler:
-            //      guard for success;
-            //      access Parse for your own student location -> append into your array of student locations
-            //      access the latest 100 student locations -> append into your array of student locations
-            //      segues to the next view
         }
         
         ParseClient.sharedInstance().getStudentLocations { (studentLocation, error) in
@@ -52,7 +44,7 @@ class LoginViewController: UIViewController {
     
     
     
-    /*
+
     func connectionFailureAlert() {
         let alertController = UIAlertController(title: "Network Failure", message: "You were unable to connect to the network", preferredStyle: .alert)
         let networkFailureNotice = UIAlertAction(title: "network Failure", style: .default, handler: nil)
@@ -60,95 +52,9 @@ class LoginViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
  
-    
-    func taskForPostSessionID() {
-        
-        // Set the parameters
-        
-        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        print("\n{\"udacity\": {\"username\": \"\(usernameTextField.text!)\", \"password\": \"\(passwordTextField.text!)\"}}\n")
-        request.httpBody = "{\"udacity\": {\"username\": \"\(usernameTextField.text!)\", \"password\": \"\(passwordTextField.text!)\"}}".data(using: String.Encoding.utf8)
-        let session = URLSession.shared
-        let task = session.dataTask(with: request as URLRequest) { data, response, error in
-            
-            // if an error occurs, print it and re-enable the UI
-            func displayError(_ error: String, debugLabelText: String? = nil) {
-                print(error)
-                performUIUpdatesOnMain {
-                    //self.setUIEnabled(true)
-                    //self.debugTextLabel.text = "Login Failed (Login Step)."
-                }
-            }
-            
-            /* GUARD: Was there an error? */
-            guard (error == nil) else {
-                displayError("There was an error with your request: \(error!)")
-                //self.connectionFailureAlert()
-                return
-            }
-            
-            /* GUARD: Did we get a successful 2XX response? */
-            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
-                displayError("Your request returned a status code other than 2xx!")
-                //self.connectionFailureAlert()
-                return
-            }
-            
-            /* GUARD: Was there any data returned? */
-            guard let data = data else {
-                displayError("No data was returned by the request!")
-                return
-            }
-            
-            let range = Range(5..<data.count)
-            let newData = data.subdata(in: range) /* subset response data! */
-            print(NSString(data: newData, encoding: String.Encoding.utf8.rawValue)!)
-            
-            /* 5. Parse the data */
-            let parsedResult: [String:AnyObject]!
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: newData, options: .allowFragments) as! [String:AnyObject]
-            } catch {
-                print("Could not parse the data as JSON: '\(newData)'")
-                return
-            }
-            
-            guard let session = parsedResult![UdacityClient.UdacityResponseKeys.Session] as? [String:AnyObject] else {
-                displayError("Cannot find session dictionary in '\(parsedResult)'")
-                return
-            }
-            
-            guard let sessionID = session[UdacityClient.UdacityResponseKeys.SessionID] as? String else {
-                displayError("Cannot find key '\(UdacityClient.UdacityResponseKeys.SessionID)' in '\(session)'")
-                return
-            }
-            
-            guard let account = parsedResult![UdacityClient.UdacityResponseKeys.Account] as? [String:AnyObject] else {
-                displayError("Cannot find account dictionary in '\(parsedResult)'")
-                return
-            }
-            
-            guard let accountKey = account[UdacityClient.UdacityResponseKeys.AccountKey] as? String else {
-                displayError("Cannot find key '\(UdacityClient.UdacityResponseKeys.AccountKey)' in '\(account)'")
-                return
-            }
-            
-            /* 6. Use the data! */
-            print(accountKey)
-            print(sessionID)
-        }
-        
-        task.resume()
-    }
-
-    deinit {
+        deinit {
         print("The LoginViewController was deinitialized")
     }
- */
-    
 }
 
 extension LoginViewController {

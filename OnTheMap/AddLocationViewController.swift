7ef316ae0d1d178a5427fdcb99d41848bca49f60
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-28 16:47:45 -0700

    Add AddLocationViewController

---
+++ OnTheMap/AddLocationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Tomas Sidenfaden on 9/28/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+}

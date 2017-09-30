ec6f9e40798abf2ab646583768e8b255e392c782
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-09-29 17:50:26 -0700

    Improve LoginViewController UI

--- OnTheMap/ParseClient.swift
+++ OnTheMap/ParseClient.swift
@@ -70,7 +70,7 @@ class ParseClient: NSObject {
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"Fickletits\", \"lastName\": \"McGinty\",\"mapString\": \"Palm Desert, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}".data(using: String.Encoding.utf8)
+        request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"Stinky\", \"lastName\": \"Pete\",\"mapString\": \"Palm Desert, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}".data(using: String.Encoding.utf8)
         
         let task = session.dataTask(with: request as URLRequest) { data, response, error in
             
